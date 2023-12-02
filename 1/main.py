73840b66c2fe0ba983083d09175c01a5f55612b5
SJ <steve@johnson.pizza>
2023-12-02 12:58:55 -0600

    Challenge 1 complete

---
+++ 1/main.py
@@ -0,0 +1,16 @@
+import re
+import string
+
+line_digits = []
+
+with open('input_data.txt') as input:
+  for line in input:
+    filterobj = filter(str.isdigit, line)
+    only_digits = str("".join(filterobj))
+    if (only_digits == ""):
+      pass
+    else:
+      first_and_last = only_digits[0] + only_digits[-1]
+      line_digits.append(int(first_and_last))
+
+print("The sum of all numbers extracted from rows is " + str(sum(line_digits)))
